387657e7b9caf60f6c4d39baaaf9d1c037c9341d
DylanPiriou <dylan.piriou@hotmail.com>
2024-05-04 01:29:37 +0200

    feat: header created

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <nav className='w-full flex items-center justify-center'>
+        <ul className='w-full flex items-center justify-evenly text-white'>
+            <li><a href="/">Accueil</a></li>
+            <li><a href="/">Profil</a></li>
+            <li><a href="/">Réglages</a></li>
+            <li><a href="/">Communauté</a></li>
+        </ul>
+    </nav>
+  )
+}
---
+++ frontend/src/components/SiteLogo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Logo from '@/assets/SportSeeLogo.svg'
+
+export default function SiteLogo() {
+  return (
+    <a href="">
+        <img src={Logo} alt="" />
+    </a>
+  )
+}
